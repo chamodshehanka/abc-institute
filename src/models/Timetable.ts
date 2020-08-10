c9cb810410ce5b84f68dbf80b97225e5750e39c7
Chamod Shehanka <hcsperera@outlook.com>
2020-08-10 14:35:07 +0530

    Timetable list done

--- src/models/Timetable.ts
+++ src/models/Timetable.ts
@@ -1,3 +1,4 @@
 export interface Timetable {
   _id: string;
+  name: string;
 }
--- src/routes/AppRoutes.ts
+++ src/routes/AppRoutes.ts
@@ -1,7 +1,9 @@
 import TestPage from '../screens/TestScreen';
 import ManageWorkingDaysScreen from '../screens/WorkingDays/ManageWorkingDaysScreen';
+import TimetableScreen from '../screens/Timetable/TimetableScreen';
 
 export const routes = [
   { path: '', component: ManageWorkingDaysScreen },
   { path: 'manage-working-days', component: ManageWorkingDaysScreen },
+  { path: 'manage-timetables', component: TimetableScreen },
 ];
