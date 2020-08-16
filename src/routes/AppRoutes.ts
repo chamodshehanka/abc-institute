2642c8af3761d278c52387c356bd49d9bd164567
Randud <randu612@gmail.com>
2020-08-16 13:50:41 +0530

    Student component created

--- src/routes/AppRoutes.ts
+++ src/routes/AppRoutes.ts
@@ -1,9 +1,10 @@
 import TestPage from '../screens/TestScreen';
 import ManageWorkingDaysScreen from '../screens/WorkingDays/ManageWorkingDaysScreen';
 import TimetableScreen from '../screens/Timetable/TimetableScreen';
-
+import StudentHomeScreen from '../screens/Student/StudentHomeScreen';
 export const routes = [
-  { path: '', component: ManageWorkingDaysScreen },
+  { path: '', component: StudentHomeScreen },
   { path: 'manage-working-days', component: ManageWorkingDaysScreen },
   { path: 'manage-timetables', component: TimetableScreen },
+  { path: 'StudentHomeScreen', component: StudentHomeScreen },
 ];
