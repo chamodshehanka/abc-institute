86827d7c16ad0353fbdaac2727ba2f647100935f
Chamod Shehanka <hcsperera@outlook.com>
2020-08-17 13:32:14 +0530

    Working Days form

--- src/api/interfaces.ts
+++ src/api/interfaces.ts
@@ -2,11 +2,48 @@ import { WorkingDays } from '../models/WorkingDays';
 
 export interface WorkingDaysCreateData {
   name: string;
+  workingHours: {
+    hours: Number;
+    mins: Number;
+  };
+  selectedDays: {
+    monday: Boolean;
+    tuesday: Boolean;
+    wednesday: Boolean;
+    thursday: Boolean;
+    friday: Boolean;
+    saturday: Boolean;
+    sunday: Boolean;
+  };
+  prefferedTimeSlots: {
+    thirty: Boolean;
+    sixty: Boolean;
+  };
+}
+
+export interface WorkingDaysUpdateData {
+  _id: string;
+  name: string;
+  workingHours: {
+    hours: Number;
+    mins: Number;
+  };
+  selectedDays: {
+    monday: Boolean;
+    tuesday: Boolean;
+    wednesday: Boolean;
+    thursday: Boolean;
+    friday: Boolean;
+    saturday: Boolean;
+    sunday: Boolean;
+  };
+  prefferedTimeSlots: {
+    thirty: Boolean;
+    sixty: Boolean;
+  };
 }
 
 export interface WorkingDaysListResponse {
   success: Boolean;
   data: WorkingDays[];
 }
-
-// export interface Working
