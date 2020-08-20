12ddb2a3f4b14d9364416666f13ca624b49c08cd
Randu Lanka <42910524+Randud@users.noreply.github.com>
2020-08-20 17:53:07 +0530

    delete function added (#25)
    * Home UI card updated
    
    * Programe delete function added

--- src/components/Student/StudentYearTable.tsx
+++ src/components/Student/StudentYearTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { YearSemester } from "../../models/yearSemester";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
@@ -11,6 +11,12 @@ import Paper from "@material-ui/core/Paper";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import Button from "@material-ui/core/Button";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Dialog from "@material-ui/core/Dialog";
+// import {deleteYearSemester} from "../../api/student/year.request";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
 
 const useStyles = makeStyles({
   table: {
@@ -32,6 +38,24 @@ const ManageYearTable: React.SFC<ManageYearProps> = ({
   yearSemester,
 }: ManageYearProps) => {
   const classes = useStyles();
+
+  const [deleteDialog, setDeleteDialog] = useState(false);
+
+  const handleDeleteDialogOpen = () => {
+    setDeleteDialog(true);
+  };
+
+  const handleDeleteDialogClose = () => {
+    setDeleteDialog(false);
+  };
+
+  // const handleDeleteAction = () => {
+  //   deleteYearSemester(yearSemester?._id)
+  //     .then((res) => {
+  //       console.log(res);
+  //     })
+  //     .catch((err) => console.error(err));
+  // };
 
   return (
     <>
@@ -63,7 +87,7 @@ const ManageYearTable: React.SFC<ManageYearProps> = ({
                 </TableCell>
                 <TableCell align="right">
                   {" "}
-                  <Button>
+                  <Button onClick={handleDeleteDialogOpen}>
                     <DeleteIcon />
                   </Button>
                 </TableCell>
@@ -71,6 +95,30 @@ const ManageYearTable: React.SFC<ManageYearProps> = ({
             ))}
           </TableBody>
         </Table>
+        <Dialog
+          open={deleteDialog}
+          onClose={handleDeleteDialogClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Delete Working Day"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {/* Are you sure to delete {yearSemester?.year} ? */}
+            </DialogContentText>
+          </DialogContent>
+
+          <DialogActions>
+            {/* <button className="btn btn-primary" onClick={handleDeleteAction}>
+              Yes
+            </button>
+            <button className="btn btn-danger" onClick={handleDeleteDialogOpen}>
+              Cancel
+            </button> */}
+          </DialogActions>
+        </Dialog>
       </TableContainer>
     </>
   );
