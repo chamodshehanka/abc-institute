f54e26972f1f107ff3d9498a5af679896668c565
deSamds <53430575+deSamds@users.noreply.github.com>
2020-09-04 18:23:52 +0530

    changes updated (#37)

--- src/screens/Locations/LocationScreen.tsx
+++ src/screens/Locations/LocationScreen.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 //import { useState } from "react";
-//import ManageBuilding from "../../components/Buildings/ManageBuilding";
-
-//import ManageLocations from "../../components/Rooms/ManageLocations";
+import ManageBuilding from "../../components/Buildings/ManageBuilding";
+import ManageRooms from "../../components/Rooms/ManageRooms";
 import Container from "@material-ui/core/Container";
-//import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
-//import { useGetBuildings } from "../../queries/useGetBuildings";
-
+import { useGetBuildings } from "../../queries/useGetBuildings";
 import AddBuildingForm from "../../components/Buildings/AddBuildingForm";
 
 // import AddRoom from "../../components/Rooms/AddRoom";
@@ -15,70 +12,61 @@ import AddBuildingForm from "../../components/Buildings/AddBuildingForm";
 // import DeleteRoom from "../../components/Rooms/DeleteRoom";
 
 const LocationsScreen: React.SFC = () => {
-  // const { data = [], status } = useGetBuildings();
+  const { data = [] } = useGetBuildings();
 
   return (
-    <div>
-      <React.Fragment>
-        <Container
+    <>
+      <h3 style={{ textAlign: "center" }}>Locations</h3>
+      <div
+        style={{
+          paddingLeft: "40px",
+          paddingRight: "40px",
+          paddingTop: "10px",
+        }}
+      >
+        <Typography
+          component="div"
           style={{
-            backgroundColor: "#DEE9FF",
-            height: "800px",
+            backgroundColor: "white",
+            height: 680,
+            borderRadius: 30,
           }}
         >
-          <div>
-            <Container fixed>
-              <br />
-              <h3 style={{ textAlign: "center" }}>Locations</h3>
-              <br />
-              <Typography
-                component="div"
-                style={{
-                  backgroundColor: "white",
-                  height: 680,
-                  borderRadius: 30,
-                }}
-              >
-                <br />
-                <Container
-                  style={{
-                    borderStyle: "solid",
-                    borderColor: "black",
-                    borderRadius: "30px",
-                    width: "95%",
-                    height: "300px",
-                    marginLeft: "30px",
-                  }}
-                >
-                  <h4 style={{ marginTop: 10, paddingLeft: 30 }}>
-                    Manage Buildings <AddBuildingForm />
-                  </h4>
+          <br />
+          <Container
+            style={{
+              borderStyle: "solid",
+              borderColor: "black",
+              borderRadius: "30px",
+              width: "95%",
+              height: "300px",
+              marginLeft: "30px",
+            }}
+          >
+            <h4 style={{ marginTop: 10, paddingLeft: 30 }}>
+              Manage Buildings <AddBuildingForm />
+            </h4>
 
-                  {/* <ManageBuilding  buildings = {data} /> */}
-                </Container>
-                <br />
-                <Container
-                  style={{
-                    borderStyle: "solid",
-                    borderColor: "black",
-                    borderRadius: "30px",
-                    width: "95%",
-                    height: "300px",
-                    marginLeft: "30px",
-                  }}
-                >
-                  <h4 style={{ marginTop: 10, paddingLeft: 30 }}>
-                    Manage Locations
-                  </h4>
+            <ManageBuilding buildings={data} />
+          </Container>
+          <br />
+          <Container
+            style={{
+              borderStyle: "solid",
+              borderColor: "black",
+              borderRadius: "30px",
+              width: "95%",
+              height: "300px",
+              marginLeft: "30px",
+            }}
+          >
+            <h4 style={{ marginTop: 10, paddingLeft: 30 }}>Manage Locations</h4>
 
-                  {/* <ManageLocations /> */}
-                </Container>
-              </Typography>
-            </Container>
-          </div>
-        </Container>
-      </React.Fragment>
-    </div>
+            <ManageRooms />
+          </Container>
+        </Typography>
+      </div>
+    </>
   );
 };
 
